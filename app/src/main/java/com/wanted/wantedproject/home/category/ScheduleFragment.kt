f75c4c220e29430bc96e0beafb88d5a7674159f9
ddyeon <cab05102@naver.com>
2021-11-14 15:30:06 +0900

    - 카테고리 개수 추가  - 가입시 카테고리 선택 페이지 추가

--- app/src/main/java/com/wanted/wantedproject/home/category/ScheduleFragment.kt
+++ app/src/main/java/com/wanted/wantedproject/home/category/ScheduleFragment.kt
@@ -1,4 +1,4 @@
-package com.wanted.wantedproject.home
+package com.wanted.wantedproject.home.category
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.wanted.wantedproject.R
 
-class ClassFragment : Fragment() {
+class ScheduleFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_class, container, false)
+        return inflater.inflate(R.layout.fragment_schedule, container, false)
     }
 
 }
